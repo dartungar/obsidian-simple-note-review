8b6f96e583e803a99d5ceb864373a1590c406916
dartungar <dartungar@gmail.com>
2023-12-02 21:22:43 +0400

    noteset stats modal data is now displayed as a table

--- src/UI/noteset/noteSetInfoModal.ts
+++ src/UI/noteset/noteSetInfoModal.ts
@@ -11,13 +11,27 @@ export class NoteSetInfoModal extends Modal {
         this.service.updateNoteSetDisplayNameAndDescription(this.noteSet);
         await this.service.updateNoteSetStats(this.noteSet);
         const { contentEl } = this;
+        
         contentEl.createEl("h3", {text: `Note set "${this.noteSet.displayName}"`});
         contentEl.createDiv({text: `This query ${this.noteSet.description}.`});
         contentEl.createEl("br");
-        contentEl.createEl("p", {text: `Total notes: ${this.noteSet.stats.totalCount}`});
-        contentEl.createEl("p", {text: `Reviewed in last 7 days: ${this.noteSet.stats.reviewedLastSevenDaysCount}`});
-        contentEl.createEl("p", {text: `Reviewed in last 30 days: ${this.noteSet.stats.reviewedLastThirtyDaysCount}`});
-        contentEl.createEl("p", {text: `Not reviewed yet: ${this.noteSet.stats.notRewiewedCount}`});
+        let tableEl = contentEl.createEl("table");
+        let tbodyEl = tableEl.createEl("tbody");
+
+        this.addTableRow(tbodyEl, "Total notes", this.noteSet.stats.totalCount);
+        this.addTableRow(tbodyEl, "Reviewed in last 7 days", this.noteSet.stats.reviewedLastSevenDaysCount);
+        this.addTableRow(tbodyEl, "Reviewed in last 30 days", this.noteSet.stats.reviewedLastThirtyDaysCount);
+        this.addTableRow(tbodyEl, "Not reviewed yet", this.noteSet.stats.notRewiewedCount);
+      }
+
+      private addTableRow(tbodyEl: HTMLElement, name: string, value: string | number): void {
+        let valueStr = typeof value === 'number' ? value.toString() : value;
+
+        let rowEl = tbodyEl.createEl("tr");
+        let nameRow = rowEl.createEl("td");
+        nameRow.setText(name);
+        nameRow.style.paddingRight = "1rem";
+        rowEl.createEl("td").setText(valueStr);
       }
     
       onClose() {
